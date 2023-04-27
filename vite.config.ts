1b26aa18b30204118d7f2e2137075792fb5b2368
Doan Sy Hung <hungdoansy@gmail.com>
2023-04-27 22:25:40 +0700

    chore(deps): use vite-plugin-checker instead of vite-plugin-eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
-import eslint from "vite-plugin-eslint"
+import checker from "vite-plugin-checker"
 import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [eslint(), react(), tsconfigPaths()],
+    plugins: [
+        checker({
+            typescript: true,
+            eslint: {
+                lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+            },
+            overlay: false,
+        }),
+        react(),
+        tsconfigPaths(),
+    ],
 })
